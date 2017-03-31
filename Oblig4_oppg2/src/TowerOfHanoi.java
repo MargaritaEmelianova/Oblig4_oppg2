fa28152a3372c6a10c9c19cfa802327cb5652905
MargaritaEm <mem004@post.uit.no>
2017-03-31 22:47:59 +0200

    Added the counter for the method call

--- Oblig4_oppg2/src/TowerOfHanoi.java
+++ Oblig4_oppg2/src/TowerOfHanoi.java
@@ -1,38 +1,40 @@
 import java.util.Scanner;
 
 public class TowerOfHanoi {
+    static int count =0;
 
-	
-	
 	static int steg = 0;
-	public static void main(String[] args) {
 	
-		Scanner input = new Scanner (System.in);
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
 		System.out.print("Gi inn antall plater: ");
 		int n = input.nextInt();
-		
-		// Finner løsningen rekursivt
+
+		// Finner løsningen rekursiv
 		System.out.println("Steggene er: ");
-		flyttPlater(n, 'A', 'B','C' );
-		
-		System.out.print( "Antall steg: " + steg);
+		flyttPlater(n, 'A', 'B', 'C');
+
+		System.out.print("Antall steg: " + steg);
+		System.out.print("\nMetoden ble kalt " + count + " ganger");
 		input.close();
 	}
 
 	public static void flyttPlater(int n, char fromTower, char toTower, char auxTower) {
-		
+    
 		steg++;
-		if ( n == 1) //stop betingelse
+		count++;
+		if (n == 1) // stop betingelse
 			System.out.println("Flytt plate " + n + " fra " + fromTower + " til " + toTower);
 		else {
 			flyttPlater(n - 1, fromTower, auxTower, toTower);
 			System.out.println("Flytt plate " + n + " fra " + fromTower + " til " + toTower);
-			flyttPlater(n-1, auxTower,toTower, fromTower);
+			flyttPlater(n - 1, auxTower, toTower, fromTower);
+			
+			
+			
 		}
-		
-	}
-	
+
 	}
 
-
-
+}

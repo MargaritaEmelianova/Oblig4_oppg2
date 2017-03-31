e7137baf03f7241754bb6d6bc8ed296f102aeb5a
MargaritaEm <mem004@post.uit.no>
2017-03-31 19:57:43 +0200

    Have added counter for moves

--- Oblig4_oppg2/src/TowerOfHanoi.java
+++ Oblig4_oppg2/src/TowerOfHanoi.java
@@ -2,26 +2,32 @@ import java.util.Scanner;
 
 public class TowerOfHanoi {
 
+	
+	
+	static int steg = 0;
 	public static void main(String[] args) {
 	
 		Scanner input = new Scanner (System.in);
-		System.out.print("Gi inn antall disker: ");
+		System.out.print("Gi inn antall plater: ");
 		int n = input.nextInt();
 		
 		// Finner løsningen rekursivt
-		System.out.println("Flyttingene er: ");
-		flyttDisker(n, 'A', 'B','C' );
+		System.out.println("Steggene er: ");
+		flyttPlater(n, 'A', 'B','C' );
 		
+		System.out.print( "Antall steg: " + steg);
 		input.close();
 	}
 
-	public static void flyttDisker(int n, char fromTower, char toTower, char auxTower) {
+	public static void flyttPlater(int n, char fromTower, char toTower, char auxTower) {
+		
+		steg++;
 		if ( n == 1) //stop betingelse
-			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
+			System.out.println("Flytt plate " + n + " fra " + fromTower + " til " + toTower);
 		else {
-			flyttDisker(n - 1, fromTower, auxTower, toTower);
-			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
-			flyttDisker(n-1, auxTower,toTower, fromTower);
+			flyttPlater(n - 1, fromTower, auxTower, toTower);
+			System.out.println("Flytt plate " + n + " fra " + fromTower + " til " + toTower);
+			flyttPlater(n-1, auxTower,toTower, fromTower);
 		}
 		
 	}
